f50714dd2575473a9b222b1694d4f4019ea4dc95
Yeming <yluo3421@gmail.com>
2022-12-01 22:56:09 -0500

    new branch made

--- scripts/lintcode.js
+++ scripts/lintcode.js
@@ -6,7 +6,7 @@ function findLanguage() {
 };
 // doesnt work for this webpage
 let new_way = document.getElementsByClassName("lanauage-python"); 
-console.log(new_way);
+// console.log(new_way);
 // can find the element but cannot find the children
 // maybe we can directly copy it to the README.md
 
@@ -14,12 +14,19 @@ new_way = document.getElementsByTagName("code");
 // [span, span, span, span, span, span, span, span]
 // map all span's inner text
 // will get all codes in text
-new_way[0].childNodes.map(x => console.log(x));
+// new_way[0].childNodes.map(x => console.log(x));
 
 // doesnt work anymore
 let old_way = document.getElementsByClassName('status-column__3SUg');
 console.log(old_way);
-
+button_class_name = "px-3 py-1.5 font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex text-label-r bg-green-s dark:bg-dark-green-s hover:bg-green-3 dark:hover:bg-dark-green-3 rounded-lg"
+submit_button = document.getElementsByClassName(button_class_name)
+console.log(submit_button)
+submit_button.addEventListener("click",handleClick)
+function handleClick(event) {
+    console.log(event.target)
+    console.log("clicked")
+}
 
 
 
