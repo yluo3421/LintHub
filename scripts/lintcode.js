d80e347fa58b66e4301ffd07a47fa5e947beb069
Yeming <yluo3421@gmail.com>
2022-12-03 21:40:41 -0500

    code grab successful by setTimeOut

--- scripts/lintcode.js
+++ scripts/lintcode.js
@@ -1,32 +1,33 @@
-let difficulty = "";
-let uploadState = { uploading: false }
-/* This function is to get file extension for submission */
-function findLanguage() {
+console.log("lintcode.js working")
+
+document.addEventListener('DOMContentLoaded', function run(){
+    console.log('After loading the page')
+    setTimeout(grabButton,2000)
     
-};
-// doesnt work for this webpage
-let new_way = document.getElementsByClassName("lanauage-python"); 
-// console.log(new_way);
-// can find the element but cannot find the children
-// maybe we can directly copy it to the README.md
+})
+function grabButton() {
+    let button_class_name = "px-3 py-1.5 font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex text-label-r bg-green-s dark:bg-dark-green-s hover:bg-green-3 dark:hover:bg-dark-green-3 rounded-lg"
+    let submit_button = document.getElementsByClassName(button_class_name)[0]
+    console.log(submit_button)
+    submit_button.addEventListener("click",handleClick)
+    setTimeout(grabCode, 5000)
+}
+function grabCode() {
+    let element = document.getElementsByClassName("mb-6")
+    console.log(element)
+    let solution_container = element
+    let solution_text = solution_container[1].innerText
+    console.log(solution_container)
+    console.log(solution_text)
+}
+console.log(window.location.href)
 
-new_way = document.getElementsByTagName("code");
-// [span, span, span, span, span, span, span, span]
-// map all span's inner text
-// will get all codes in text
-// new_way[0].childNodes.map(x => console.log(x));
+//!!!!!!!!!!!!!!MutationObserver()
 
-// doesnt work anymore
-let old_way = document.getElementsByClassName('status-column__3SUg');
-console.log(old_way);
-button_class_name = "px-3 py-1.5 font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex text-label-r bg-green-s dark:bg-dark-green-s hover:bg-green-3 dark:hover:bg-dark-green-3 rounded-lg"
-submit_button = document.getElementsByClassName(button_class_name)
-console.log(submit_button)
-submit_button.addEventListener("click",handleClick)
+
 function handleClick(event) {
     console.log(event.target)
     console.log("clicked")
 }
-
 
 
