64067b002b16ebf2788f52635641ab8ede31eb27
Yeming <yluo3421@gmail.com>
2022-12-05 09:18:53 -0500

    found out how to get client ID, client secret, code, temp token. Decided to put storage for later. XHR doesnt allow access from leetcode to github.com

--- scripts/lintcode.js
+++ scripts/lintcode.js
@@ -1,23 +1,23 @@
-console.log("lintcode.js working")
+
 
 document.addEventListener('DOMContentLoaded', function run(){
-    console.log('After loading the page')
+   
     setTimeout(grabButton,2000)
     
 })
 function grabButton() {
     let button_class_name = "px-3 py-1.5 font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex text-label-r bg-green-s dark:bg-dark-green-s hover:bg-green-3 dark:hover:bg-dark-green-3 rounded-lg"
     let submit_button = document.getElementsByClassName(button_class_name)[0]
-    console.log(submit_button)
+    
     submit_button.addEventListener("click",handleClick)
     setTimeout(grabCode, 5000)
 }
 function grabCode() {
     let element = document.getElementsByClassName("mb-6")
-    console.log(element)
+    
     let solution_container = element
     let solution_text = solution_container[1].innerText
-    console.log(solution_container)
+    
     console.log(solution_text)
 }
 console.log(window.location.href)
@@ -45,6 +45,79 @@ function handleClick(event) {
         },
 */ 
 // msg:
+
+// try request token
+// step 1, use client ID and client secret from LeetHub to get code
+// step 2, use code to get token
+// pass token to upload
+this.ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+this.AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
+this.CLIENT_ID = 'beb4f0aa19ab8faf5004';
+this.CLIENT_SECRET = '843f835609c7ef02ef0f2f1645bc49514c0e65a6';
+this.REDIRECT_URL = 'https://github.com/'; // for example, https://github.com
+this.SCOPES = ['repo'];
+let code = ""
+function requestCode() {
+    const data = new FormData();
+    data.append('client_id', this.CLIENT_ID);
+    data.append('redirect_uri', this.REDIRECT_URL);
+    data.append('scope', this.SCOPES)
+    // data.append('state', "NULL")
+    console.log("requestCode got into here!")
+    const xhr = new XMLHttpRequest();
+    let tempcode = "";
+    xhr.addEventListener('readystatechange', function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            tempcode = xhr.responseText
+            that.finish(
+              console.log(xhr.responseText),
+              
+            );
+          } else {
+            chrome.runtime.sendMessage({
+              closeWebPage: true,
+              isSuccess: false,
+            });
+          }
+        }
+    });
+    xhr.open('GET', this.ACCESS_TOKEN_URL, true);
+    xhr.send(data);
+    return tempcode
+}
+code = requestCode()
+console.log(code)
+function requestToken(code) {
+    
+    const data = new FormData();
+    data.append('client_id', this.CLIENT_ID);
+    data.append('client_secret', this.CLIENT_SECRET);
+    data.append('code', code);
+    console.log("requestToken got here")
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', function () {
+        
+      if (xhr.readyState === 4) {
+        console.log(xhr.status)
+        if (xhr.status === 200) {
+          that.finish(
+            xhr.responseText.match(/access_token=([^&]*)/)[1],
+            console.log("temp token got"),
+            console.log(xhr.responseText),
+          );
+        } else {
+          chrome.runtime.sendMessage({
+            closeWebPage: true,
+            isSuccess: false,
+          });
+        }
+      }
+    });
+    xhr.open('POST', this.ACCESS_TOKEN_URL, true);
+    xhr.send(data);
+}
+requestToken(code)
 const token = "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";
 const personal_hook = "yluo3421/Leetcode-Solutions";
 const test_sha = {"0026-remove-duplicates-from-sorted-array0026-remove-duplicates-from-sorted-array.py": "0e0cffed07d03950aa5b53c3aa7b705fa621a5c0"};
@@ -70,6 +143,8 @@ function upload(token, hook, code, directory, filename, sha, msg) {
             console.log(s)
         }))
         if (xhr.readyState === 4) {
+            console.log("!!!!!!!!Show XHR")
+            console.log(xhr)
             console.log("readyState === 4")
             if (xhr.status === 200 || xhr.status === 201) {
                 console.log("success upload to GitHub")
@@ -82,7 +157,13 @@ function upload(token, hook, code, directory, filename, sha, msg) {
     xhr.open('PUT', URL, true);
     xhr.setRequestHeader('Authorization', `token ${token}`);
     xhr.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-    xhr.send(data);
+    try {
+        xhr.send(data);
+    } catch (error) {
+        console.log("possible 422 error message")
+        console.log(error)
+    }
+    
 }
 
 
